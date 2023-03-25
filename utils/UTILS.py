69fe5eff2c56a886eed769c68605aaad8278d04d
Yurui Zhu <69180896+zhuyr97@users.noreply.github.com>
2023-03-25 15:13:58 +0800

    Add files via upload

---
+++ utils/UTILS.py
@@ -0,0 +1,215 @@
+import math,cv2
+import numpy as np
+
+import torch
+from torchvision.utils import make_grid
+
+from PIL import Image
+#from skimage.metrics import peak_signal_noise_ratio
+#from skimage.metrics import structural_similarity as compare_ssim
+
+
+def tensor2im(image_tensor):
+    image_numpy = image_tensor[0].cpu().float().numpy()
+    image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 0.5) * 255.0
+    return image_numpy
+
+
+def compute_psnr(images, labels):
+
+    batch, _, _, _ = images.size()
+    PSNR = 0
+    for i in range(batch):
+        PSNR += psnr(images[i] * 255, labels[i] * 255)
+
+    PSNR = PSNR / batch
+
+    return PSNR
+
+
+def psnr(img1, img2):
+
+    img1 = img1.cpu().detach().numpy()
+    img2 = img2.cpu().detach().numpy()
+    img1 = np.transpose(np.float64(img1), (1, 2, 0))
+    img2 = np.transpose(np.float64(img2), (1, 2, 0))
+    mse = np.mean((img1 - img2) ** 2)
+    if mse == 0:
+        return 100
+    PIXEL_MAX = 255.0
+    return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
+
+
+def tensor2img(tensor, out_type=np.uint8, min_max=(0, 1)):
+    '''
+    Converts a torch Tensor into an image Numpy array
+    Input: 4D(B,(3/1),H,W), 3D(C,H,W), or 2D(H,W), any range, RGB channel order
+    Output: 3D(H,W,C) or 2D(H,W), [0,255], np.uint8 (default)
+    '''
+    tensor = tensor.squeeze().float().cpu().clamp_(*min_max)  # clamp
+    tensor = (tensor - min_max[0]) / (min_max[1] - min_max[0])  # to range [0,1]
+    n_dim = tensor.dim()
+    if n_dim == 4:
+        n_img = len(tensor)
+        img_np = make_grid(tensor, nrow=int(math.sqrt(n_img)), normalize=False).numpy()
+        img_np = np.transpose(img_np[[2, 1, 0], :, :], (1, 2, 0))  # HWC, BGR
+    elif n_dim == 3:
+        img_np = tensor.numpy()
+        img_np = np.transpose(img_np[[2, 1, 0], :, :], (1, 2, 0))  # HWC, BGR
+    elif n_dim == 2:
+        img_np = tensor.numpy()
+    else:
+        raise TypeError(
+            'Only support 4D, 3D and 2D tensor. But received with dimension: {:d}'.format(n_dim))
+    if out_type == np.uint8:
+        img_np = (img_np * 255.0).round()
+        # Important. Unlike matlab, numpy.unit8() WILL NOT round by default.
+    return img_np.astype(out_type)
+
+def calculate_psnr_imgs(img1, img2):
+    # img1 and img2 have range [0, 255]
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+    mse = np.mean((img1 - img2)**2)
+    if mse == 0:
+        return float('inf')
+    return 20 * math.log10(255.0 / math.sqrt(mse))
+
+def single_forward(model, inp):
+    """PyTorch model forward (single test), it is just a simple warpper
+    Args:
+        model (PyTorch model)
+        inp (Tensor): inputs defined by the model
+
+    Returns:
+        output (Tensor): outputs of the model. float, in CPU
+    """
+    with torch.no_grad():
+        model_output = model(inp)
+        if isinstance(model_output, list) or isinstance(model_output, tuple):
+            output = model_output[0]
+        else:
+            output = model_output
+    output = output.data.float().cpu()
+    return output
+
+def flipx4_forward(model, inp):
+    """Flip testing with X4 self ensemble, i.e., normal, flip H, flip W, flip H and W
+    Args:
+        model (PyTorch model)
+        inp (Tensor): inputs defined by the model
+    Returns:
+        output (Tensor): outputs of the model. float, in CPU
+    """
+    # normal
+    output_f = single_forward(model, inp)
+
+    # flip W
+    output = single_forward(model, torch.flip(inp, (-1, )))
+    output_f = output_f + torch.flip(output, (-1, ))
+    # flip H
+    output = single_forward(model, torch.flip(inp, (-2, )))
+    output_f = output_f + torch.flip(output, (-2, ))
+    # flip both H and W
+    output = single_forward(model, torch.flip(inp, (-2, -1)))
+    output_f = output_f + torch.flip(output, (-2, -1))
+
+    return output_f / 4
+
+"""
+def compute_ssim(images, labels):
+
+    batch, _, _, _ = images.size()
+    SSIM = 0
+    for i in range(batch):
+
+        SSIM += ssim(images[i] * 255, labels[i] * 255)
+
+    SSIM = SSIM / batch
+    return SSIM
+
+
+def ssim(img1, img2):
+
+    img1 = img1.cpu().detach().numpy()
+    img2 = img2.cpu().detach().numpy()
+    img1 = np.transpose(np.uint8(img1), (1, 2, 0))
+    img2 = np.transpose(np.uint8(img2), (1, 2, 0))
+    ssim_value = compare_ssim(img1, img2, multichannel=True)
+
+    return ssim_value
+"""
+
+
+def imgtoimg(img):
+    img = img.data.float().cpu().numpy()
+    img = np.transpose(img, (1, 2, 0))
+    img = np.uint8((img.clip(0, 1) * 255.).round())
+
+    return img
+
+def compute_ssim(images, labels):
+
+    batch, _, _, _ = images.size()
+    SSIM = 0
+    for i in range(batch):
+
+        SSIM += calculate_ssim(images[i], labels[i])
+
+    SSIM = SSIM / batch
+    return SSIM
+
+
+def calculate_ssim(img1, img2, border=0):
+
+    '''
+    calculate SSIM
+    the same outputs as MATLAB's
+    img1, img2: [0, 255]
+    '''
+    img1 = imgtoimg(img1)
+    img2 = imgtoimg(img2)
+    if not img1.shape == img2.shape:
+        raise ValueError('Input images must have the same dimensions.')
+    h, w = img1.shape[:2]
+    img1 = img1[border:h - border, border:w - border]
+    img2 = img2[border:h - border, border:w - border]
+
+    if img1.ndim == 2:
+        return ssim(img1, img2)
+    elif img1.ndim == 3:
+        if img1.shape[2] == 3:
+            ssims = []
+            for i in range(3):
+                ssims.append(ssim(img1[:, :, i], img2[:, :, i]))
+            return np.array(ssims).mean()
+        elif img1.shape[2] == 1:
+            return ssim(np.squeeze(img1), np.squeeze(img2))
+    else:
+        raise ValueError('Wrong input image dimensions.')
+
+
+def ssim(img1, img2):
+    C1 = (0.01 * 255) ** 2
+    C2 = (0.03 * 255) ** 2
+
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+    kernel = cv2.getGaussianKernel(11, 1.5)
+    window = np.outer(kernel, kernel.transpose())
+
+    mu1 = cv2.filter2D(img1, -1, window)[5:-5, 5:-5]  # valid
+    mu2 = cv2.filter2D(img2, -1, window)[5:-5, 5:-5]
+    mu1_sq = mu1 ** 2
+    mu2_sq = mu2 ** 2
+    mu1_mu2 = mu1 * mu2
+    sigma1_sq = cv2.filter2D(img1 ** 2, -1, window)[5:-5, 5:-5] - mu1_sq
+    sigma2_sq = cv2.filter2D(img2 ** 2, -1, window)[5:-5, 5:-5] - mu2_sq
+    sigma12 = cv2.filter2D(img1 * img2, -1, window)[5:-5, 5:-5] - mu1_mu2
+
+    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) *
+                                                            (sigma1_sq + sigma2_sq + C2))
+    return ssim_map.mean()
+
+
+
